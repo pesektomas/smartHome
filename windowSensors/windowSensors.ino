2a9dac4850b5370a6aa78258572759ba05fc1611
Tomas Pesek <pesovo@gmail.com>
2017-08-11 22:29:31 +0200

    window sensors

---
+++ windowSensors/windowSensors.ino
@@ -0,0 +1,32 @@
+// Arduino mega - 54 I/O pins
+
+const byte windowAlarmCount = 6;
+const byte sensorPins[] = {0, 1, 2, 3, 4, 5};
+const byte alarmPins[] = {53, 52, 51, 50, 49, 48};
+
+void setup() {
+  for (byte i = 0; i < windowAlarmCount; i ++) {
+    initAlarm(sensorPins[i], alarmPins[i]);
+  }
+}
+
+void loop() { 
+  for (byte i = 0; i < windowAlarmCount; i ++) {
+    loopAlarm(sensorPins[i], alarmPins[i]);
+  }
+  delay(1);
+}
+
+void initAlarm(byte sensorPin, byte alarmPin) {
+  pinMode(sensorPin, INPUT);
+  pinMode(alarmPin, OUTPUT);
+  digitalWrite(sensorPin, HIGH);
+}
+
+void loopAlarm(byte sensorPin, byte alarmPin) {
+  if(digitalRead(sensorPin) == LOW){
+    digitalWrite(alarmPin, LOW);
+  } else {
+    digitalWrite(alarmPin, HIGH);
+  }
+}
